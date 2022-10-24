1ebbc18821731ca1fb97111500639f044a19f488
saviogodinho <saviogodinho2002@gmai.com>
2022-10-24 11:18:57 -0300

    party ajust and init office

--- app/src/main/java/com/example/urnaeletrnica/OfficeAdm.kt
+++ app/src/main/java/com/example/urnaeletrnica/OfficeAdm.kt
@@ -2,10 +2,14 @@ package com.example.urnaeletrnica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.urnaeletrnica.model.entities.Candidate
+import com.example.urnaeletrnica.model.entities.Office
 
 class OfficeAdm : AppCompatActivity() {
 
@@ -13,10 +17,11 @@ class OfficeAdm : AppCompatActivity() {
     private lateinit var editOfficeName: EditText;
     private lateinit var editOfficeMaxNumber: EditText;
 
-    private lateinit var btnSave: Button;
-    private lateinit var btnUpdate: Button;
-    private lateinit var recyclerView: RecyclerView;
+    private lateinit var btnSaveUpdate: Button;
 
+    private lateinit var adapter: ListOfficeAdapter;
+    private lateinit var recyclerView: RecyclerView;
+    private lateinit var officeData:MutableList<Office>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,21 +29,68 @@ class OfficeAdm : AppCompatActivity() {
 
         editOfficeName = findViewById(R.id.edit_office_name);
         editOfficeMaxNumber = findViewById(R.id.edit_office_initials);
-        btnSave = findViewById(R.id.btn_office_save);
-        btnUpdate = findViewById(R.id.btn_office_update);
+        btnSaveUpdate = findViewById(R.id.btn_office_save);
+
+        officeData = mutableListOf();
+        adapter = ListOfficeAdapter(officeData){id, office, view ->
+
+        }
 
         recyclerView = findViewById(R.id.recycler_office);
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter;
 
-        btnSave.setOnClickListener {
+
+        //fetch data
+        Thread{
+            val app = application as App
+            val dao = app.db.OfficeDao()
+
+            val response = dao.getOffices()
+
+            runOnUiThread {
+                officeData.addAll(response)
+                adapter.notifyDataSetChanged()
+            }
+
+        }.start()
+
+
+        btnSaveUpdate.setOnClickListener {
 
         }
-        btnUpdate.setOnClickListener {
 
-        }
 
     }
 
+    private inner class ListOfficeAdapter(
+        private val officeList:List<Office>,
+        private val actions:((Int,Office,View)->Unit)
+    ):RecyclerView.Adapter<ListOfficeAdapter.ListOfficeViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup,viewType: Int): ListOfficeAdapter.ListOfficeViewHolder {
+            val view = layoutInflater.inflate(R.layout.office_layout,parent,false)
+            return ListOfficeViewHolder(view)
+        }
 
+        override fun onBindViewHolder(currentViewHolder: ListOfficeAdapter.ListOfficeViewHolder, position: Int ) {
+            val currentItem = officeList[position]
+            currentViewHolder.bind(currentItem)
+        }
+
+        override fun getItemCount(): Int {
+            return officeList.size
+        }
+        private inner class ListOfficeViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+            fun bind(item: Office){
+                val txtName = itemView.findViewById<TextView>(R.id.txt_office_name);
+                val txtNumMax = itemView.findViewById<TextView>(R.id.txt_office_nummax);
+
+                txtName.text = item.name;
+                txtNumMax.text = item.numberQuant.toString();
+            }
+        }
+
+    }
 
 
 }
--- app/src/main/java/com/example/urnaeletrnica/PartysAdm.kt
+++ app/src/main/java/com/example/urnaeletrnica/PartysAdm.kt
@@ -15,6 +15,7 @@ import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -25,46 +26,51 @@ import java.util.*
 
 class PartysAdm : AppCompatActivity() {
 
-    private lateinit var editPartyName:EditText;
-    private lateinit var editPartyInitials:EditText;
-    private lateinit var editPartyNumber:EditText;
-    private lateinit var btnSaveOrUpdate:Button;
+    private lateinit var editPartyName:EditText
+    private lateinit var editPartyInitials:EditText
+    private lateinit var editPartyNumber:EditText
+    private lateinit var btnSaveOrUpdate:Button
 
-    private lateinit var recyclerView: RecyclerView;
-    private lateinit var imgToParty:ImageView;
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var imgToParty:ImageView
 
-    private lateinit var partyData:MutableList<Party>;
-    private lateinit var adapter:ListPartyAdapter;
+    private lateinit var partyData:MutableList<Party>
+    private lateinit var adapter:ListPartyAdapter
 
-    private var imgUri: Uri? = null;
-    private var partyOnFocus:Party? = null;
-    private var partyOnFocusView:ConstraintLayout? = null;
+    private var imgUri: Uri? = null
+    private var partyOnFocus:Party? = null
+    private var partyOnFocusView:ConstraintLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_partys_adm)
 
-        editPartyName = findViewById(R.id.edit_party_name);
-        editPartyInitials = findViewById(R.id.edit_party_initials);
-        editPartyNumber = findViewById(R.id.edit_party_number);
+        editPartyName = findViewById(R.id.edit_party_name)
+        editPartyInitials = findViewById(R.id.edit_party_initials)
+        editPartyNumber = findViewById(R.id.edit_party_number)
 
-        btnSaveOrUpdate = findViewById(R.id.btn_party_save);
+        btnSaveOrUpdate = findViewById(R.id.btn_party_save)
 
 
-        imgToParty = findViewById(R.id.img_party_photo);
+        imgToParty = findViewById(R.id.img_party_photo)
 
-        recyclerView = findViewById(R.id.recycler_partys);
+        recyclerView = findViewById(R.id.recycler_partys)
 
 
         partyData = mutableListOf<Party>()
-        adapter = ListPartyAdapter(partyData);
+        adapter = ListPartyAdapter(partyData){id,view,party->
+            when(id){
+                0 -> markParty(view!!,party!!)
+                1 -> deleteParty(party!!)
+            }
+        }
 
-        recyclerView.layoutManager = LinearLayoutManager(this);
-        recyclerView.adapter = adapter;
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
 
         Thread{
-            val app = application as App;
-            val dao = app.db.PartyDao();
+            val app = application as App
+            val dao = app.db.PartyDao()
 
             val response = dao.getPartRegisters()
 
@@ -80,20 +86,38 @@ class PartysAdm : AppCompatActivity() {
                 saveParty()
             else
                 updateParty()
-            btnSaveOrUpdate.setText(R.string.btn_save);
+            btnSaveOrUpdate.setText(R.string.btn_save)
         }
         imgToParty.setOnClickListener {
             selectPhoto()
         }
 
-
-
     }
+    private fun resetPartyFocus(){
+        partyOnFocus = null
+        partyOnFocusView = null
+    }
+    private fun markParty(layout: View,party: Party){
+        partyOnFocusView?.setBackgroundColor(ContextCompat.getColor(this,android.R.color.transparent))
+        if(partyOnFocus == party){
+            resetPartyFocus()
+            btnSaveOrUpdate.setText(R.string.btn_save)
+            resetForm()
+            return
+        }
+        insertDataInFom(party)
+        btnSaveOrUpdate.setText(R.string.btn_update)
+        partyOnFocusView = layout as ConstraintLayout
+        partyOnFocus = party
+        layout.setBackgroundColor(ContextCompat.getColor(this,android.R.color.holo_blue_dark) )
+    }
+
+
     private fun resetForm(){
-        editPartyName.setText("");
-        editPartyInitials.setText("");
-        editPartyNumber.setText("");
-        imgToParty.setImageResource(R.drawable.ic_add_photo);
+        editPartyName.setText("")
+        editPartyInitials.setText("")
+        editPartyNumber.setText("")
+        imgToParty.setImageResource(R.drawable.ic_add_photo)
     }
 
 
@@ -105,14 +129,14 @@ class PartysAdm : AppCompatActivity() {
     }
     private fun updateParty(){
         if(!formIsValid() && partyOnFocus == null)
-            return;
-        var imgDirectory:String? = null;
+            return
+        var imgDirectory:String? = null
         Thread{
 
             if(imgUri != null){
-                imgDirectory = saveAndGetDirPhoto(imgUri!!);
+                imgDirectory = saveAndGetDirPhoto(imgUri!!)
             }else if(partyOnFocus!!.logoPhoto != null){
-                imgDirectory = partyOnFocus!!.logoPhoto;
+                imgDirectory = partyOnFocus!!.logoPhoto
             }
             val party = Party(
                 id = partyOnFocus!!.id,
@@ -120,25 +144,25 @@ class PartysAdm : AppCompatActivity() {
                 logoPhoto = imgDirectory,
                 initials =  editPartyInitials.text.toString().trim(),
                 number = editPartyNumber.text.toString().trim(),
-                timeStamp = System.currentTimeMillis());
+                timeStamp = System.currentTimeMillis())
 
-            val app = application as App;
+            val app = application as App
             val dao = app.db.PartyDao()
 
 
             if( (partyOnFocus!!.logoPhoto != null) && ((imgDirectory == null) || (partyOnFocus!!.logoPhoto != imgDirectory)) )
-                removePhotoFile(partyOnFocus!!.logoPhoto!!);
+                removePhotoFile(partyOnFocus!!.logoPhoto!!)
 
-            partyData[partyData.indexOf(partyOnFocus!!)] = party;
-            dao.updateParty(party);
-            partyOnFocus = null;
-            partyOnFocusView!!.setBackgroundColor(resources.getColor(android.R.color.transparent));
-            partyOnFocusView = null;
-            imgUri = null;
+            partyData[partyData.indexOf(partyOnFocus!!)] = party
+            dao.updateParty(party)
+            partyOnFocus = null
+            partyOnFocusView!!.setBackgroundColor(resources.getColor(android.R.color.transparent))
+            partyOnFocusView = null
+            imgUri = null
 
             runOnUiThread{
                 adapter.notifyDataSetChanged()
-                resetForm();
+                resetForm()
             }
 
         }.start()
@@ -146,22 +170,22 @@ class PartysAdm : AppCompatActivity() {
     }
     private fun saveParty(){
         if(!formIsValid())
-            return;
-        var imgDirectory:String? = null;
+            return
+        var imgDirectory:String? = null
 
         Thread{
 
             if(imgUri != null){
-                imgDirectory = saveAndGetDirPhoto(imgUri!!);
+                imgDirectory = saveAndGetDirPhoto(imgUri!!)
             }
             val party = Party(
                 name = editPartyName.text.toString().trim(),
                 logoPhoto = imgDirectory,
                 initials =  editPartyInitials.text.toString().trim(),
                 number = editPartyNumber.text.toString().trim(),
-                timeStamp = System.currentTimeMillis());
+                timeStamp = System.currentTimeMillis())
 
-            val app = application as App;
+            val app = application as App
             val dao = app.db.PartyDao()
 
             partyData.add(party)
@@ -169,7 +193,7 @@ class PartysAdm : AppCompatActivity() {
 
             runOnUiThread{
                 adapter.notifyDataSetChanged()
-                resetForm();
+                resetForm()
             }
 
         }.start()
@@ -177,13 +201,13 @@ class PartysAdm : AppCompatActivity() {
 
     private fun deleteParty(party:Party){
         Thread{
-            val app = application as App;
+            val app = application as App
             val dao = app.db.PartyDao()
 
             if (party.logoPhoto != null)
-                removePhotoFile(party.logoPhoto);
-            partyData.remove(party);
-            dao.deleteParty(party);
+                removePhotoFile(party.logoPhoto)
+            partyData.remove(party)
+            dao.deleteParty(party)
 
             runOnUiThread {
 
@@ -193,57 +217,60 @@ class PartysAdm : AppCompatActivity() {
     }
 
     private fun insertDataInFom(party: Party){
-        editPartyName.setText(party.name);
+        editPartyName.setText(party.name)
         editPartyInitials.setText(party.initials)
-        editPartyNumber.setText(party.number);
+        editPartyNumber.setText(party.number)
 
         imgToParty.setImageURI(party.logoPhoto?.toUri())
     }
 
     private fun selectPhoto(){
-        val intent = Intent(Intent.ACTION_PICK);
-        intent.type = "image/*";
-        selectPhotoLauncher.launch(intent);
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        selectPhotoLauncher.launch(intent)
 
     }
     private var selectPhotoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
         if (result.resultCode == Activity.RESULT_OK) {
-            imgUri = result.data?.data;
-            imgToParty.setImageURI(imgUri);
+            imgUri = result.data?.data
+            imgToParty.setImageURI(imgUri)
         }
     }
     private fun saveAndGetDirPhoto(uri:Uri):String?{
         try {
-            val cw = ContextWrapper(applicationContext);
-            val directory = cw.getDir("party_photos",Context.MODE_PRIVATE);
-            val path = File(directory,"${UUID.randomUUID().toString()}.jpg");
+            val cw = ContextWrapper(applicationContext)
+            val directory = cw.getDir("party_photos",Context.MODE_PRIVATE)
+            val path = File(directory,"${UUID.randomUUID().toString()}.jpg")
 
-            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imgUri);
-            val out = FileOutputStream(path);
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imgUri)
+            val out = FileOutputStream(path)
 
-            bitmap.compress( Bitmap.CompressFormat.PNG,100,out);
-            out.close();
-            return path.absolutePath;
+            bitmap.compress( Bitmap.CompressFormat.PNG,100,out)
+            out.close()
+            return path.absolutePath
 
         }catch (error:Exception){
 
         }
-        return  null;
+        return  null
     }
     private fun removePhotoFile(photo:String){
-        val file = File(photo);
+        val file = File(photo)
         file.delete()
     }
 
-    private inner class ListPartyAdapter(private val partyList:List<Party>):RecyclerView.Adapter<ListPartyAdapter.ListPartyViewHolder>(){
+    private inner class ListPartyAdapter(
+        private val partyList:List<Party>,
+        private val actions:((Int,View?,Party?)->Unit)
+    ):RecyclerView.Adapter<ListPartyAdapter.ListPartyViewHolder>(){
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListPartyAdapter.ListPartyViewHolder {
             val view = layoutInflater.inflate(R.layout.party_layout,parent,false)
-            return ListPartyViewHolder(view);
+            return ListPartyViewHolder(view)
         }
 
         override fun onBindViewHolder(currentViewHolder: ListPartyAdapter.ListPartyViewHolder, position: Int) {
-            val currentItem = partyList[position];
-            currentViewHolder.bind(currentItem);
+            val currentItem = partyList[position]
+            currentViewHolder.bind(currentItem)
         }
 
         override fun getItemCount(): Int {
@@ -254,45 +281,33 @@ class PartysAdm : AppCompatActivity() {
             fun bind(item:Party){
 
                 val txtName = itemView.findViewById<TextView>(R.id.txt_party_name)
-                val txtInitial = itemView.findViewById<TextView>(R.id.txt_party_initial);
-                val txtNumber = itemView.findViewById<TextView>(R.id.txt_party_number);
+                val txtInitial = itemView.findViewById<TextView>(R.id.txt_party_initial)
+                val txtNumber = itemView.findViewById<TextView>(R.id.txt_party_number)
 
-                val imgPhoto = itemView.findViewById<ImageView>(R.id.img_logo_party);
+                val imgPhoto = itemView.findViewById<ImageView>(R.id.img_logo_party)
 
-                val imgDelet = itemView.findViewById<ImageView>(R.id.img_icon_delet);
+                val imgDelet = itemView.findViewById<ImageView>(R.id.img_icon_delet)
 
-                val layoutThisParty = itemView.findViewById<ConstraintLayout>(R.id.cl_party_item);
+                val layoutThisParty = itemView.findViewById<ConstraintLayout>(R.id.cl_party_item)
                 if(item.logoPhoto != null){
-                    imgPhoto.setImageURI(item.logoPhoto.toUri());
+                    imgPhoto.setImageURI(item.logoPhoto.toUri())
                 }
 
 
-                txtName.text = item.name;
-                txtInitial.text = item.initials;
-                txtNumber.text = item.number;
-
-
+                txtName.text = item.name
+                txtInitial.text = item.initials
+                txtNumber.text = item.number
 
                 layoutThisParty.setOnClickListener {
-                    partyOnFocusView?.setBackgroundColor(resources.getColor(android.R.color.transparent));
-                    if(partyOnFocus == item){
-                        partyOnFocus = null;
-                        partyOnFocusView = null;
-                        btnSaveOrUpdate.setText(R.string.btn_save);
-                        resetForm();
-                        return@setOnClickListener;
-                    }
-                    insertDataInFom(item);
-                    btnSaveOrUpdate.setText(R.string.btn_update);
-                    partyOnFocusView = layoutThisParty;
-                    partyOnFocus = item;
-                    layoutThisParty.setBackgroundColor(resources.getColor(android.R.color.holo_blue_dark) );
+
+                    actions.invoke(0, layoutThisParty, item)
 
                 }
 
                 imgDelet.setOnClickListener {
                     Log.i("teste","clickdelete")
-                    deleteParty(item);
+                    actions.invoke(1,null,item)
+
                 }
 
             }
